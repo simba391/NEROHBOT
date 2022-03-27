030e2e589b5253d2703952df768d9ee45ee414f1
AhmadBOTV3z <101960570+AhmadBOTV3z@users.noreply.github.com>
2022-03-27 17:51:00 +0800

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,8 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Source code')).buffer(), `
 𝙎𝘾 𝙊𝙍𝙄 𝘽𝙔 𝙇𝙔𝙉𝙓𝙕𝙔
-github.com/LynnXzy/scbotz-v1
-`.trim(), '©LynXzy🥀', 'Makasih', 'Thanks')
+github.com/AhmadBOTV3z/AHMADBOT
+`.trim(), '© AhmadBOT', 'Makasih', 'Thanks')
 handler.help = ['sourcecode']
 handler.tags = ['info']
 handler.command = /^(sourcecode|sc|scbot|script|github)$/i
