2d13ab700060b7928b9f2d2603a451941b199071
AhmadBOTV3z <101960570+AhmadBOTV3z@users.noreply.github.com>
2022-03-27 14:31:47 +0800

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -3,10 +3,10 @@ let { MessageType } = require('@adiwajshing/baileys')
 let handler = async(m, { conn }) => {
     let kamisato = `
 ╭═══ 〔 SEWA BOT 〕 ════
-├⬡ 𝟏 ᗰIᑎᘜᘜᑌ
-├⬡ 2 ᗰIᑎᘜᘜᑌ
-├⬡ 3 ᗰIᑎᘜᘜᑌ
-├⬡ 1 ᗷᑌᒪᗩᑎ
+├⬡  PERMANENT
+├⬡  PERMANENT
+├⬡  PERMANENT
+├⬡  PERMANENT
 └═══════════════
 𝗦𝗶𝗹𝗮𝗸𝗮𝗻 𝗸𝗹𝗶𝗸 𝗽𝗮𝗱𝗮 "𝗟𝗶𝘀𝘁 𝗛𝗮𝗿𝗴𝗮" 𝘂𝗻𝘁𝘂𝗸 𝗺𝗲𝗹𝗶𝗵𝗮𝘁 𝗹𝗶𝘀𝘁.
 ╭═══════════════════════
@@ -20,13 +20,12 @@ let handler = async(m, { conn }) => {
         buttonText: 'List Harga',
         description: kamisato,
         sections:  [{title: "Silahkan di pilih", rows: [
-        {title: '1 Minggu', description: "Rm5.00\nSewa bot 1 Minggu.", rowId:".masuk"},
-        {title: '2 Minggu', description: "Rm10.00\nSewa bot 2 Minggu.", rowId:".masuk"},
-        {title: '3 Minggu', description: "Rm15.00\nSewa bot 3 Minggu.", rowId:".masuk"},
-        {title: '1 Bulan', description: "Rm20.00\nSewa bot 1 Bulan.", rowId:".masuk"},
+        {title: 'VIA PULSA :', description: "RP5.000\nSewa bot PERMANENT.", rowId:".masuk"},
+        {title: 'VIA DANA :', description: "RP10.000\nSewa bot PERMANENT.", rowId:".masuk"},
+        {title: 'VIA GOPAY :', description: "RP10.000\nSewa bot PERMANENT.", rowId:".masuk"},
         {title: 'Trial', description: "Free\nBot Free 1 Hari.", rowId:".masuk"},
         {title: 'Owner', description: "Chat owner nya jika ada perlu.", rowId:".owner"},
-        {title: 'Rules', description: "Rules Bot LynXzy.", rowId:".snk"},
+        {title: 'Rules', description: "Rules BOT AHMAD.", rowId:".snk"},
        ] }],
         listType: 1
        }
