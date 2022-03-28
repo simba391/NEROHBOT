43d60cd2b103b20a0907a8c1f7bf25178d6e9192
AhmadBOTV3z <101960570+AhmadBOTV3z@users.noreply.github.com>
2022-03-28 20:18:03 +0800

    Update jadibot.js

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -70,9 +70,9 @@ handler.help = ['jadibot']
 handler.tags = ['jadibot']
 
 handler.command = /^jadibot$/i
-handler.premium = false
+handler.premium = true
 handler.private = true
 
-handler.limit = 5
+handler.limit = 1
 
 module.exports = handler
