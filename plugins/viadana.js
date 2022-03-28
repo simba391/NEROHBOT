edaa2f925afb98e6fe620b4fb369d9c2f2f460fd
AhmadBOTV3z <101960570+AhmadBOTV3z@users.noreply.github.com>
2022-03-28 08:19:24 +0800

    Update viadana.js

--- plugins/viadana.js
+++ plugins/viadana.js
@@ -1,17 +1,17 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `
 ╠═〘 PEMBAYARAN 〙 ═
-╠═ Untuk Pembayaran Via Digi
-╠➥ *60149431385*
+╠═ Untuk Pembayaran Via DANA
+╠➥ *6281250521455*
 ║- Owner Bot:
-║- wa.me/60189830350
-╠═〘 LYNXZY 〙 ═
+║- wa.me/6281250521455
+╠═〘 AHMADBOT 〙 ═
 `.trim()
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*Sewa Bot*', 'status@broadcast') 
 }
-handler.help = ['viadigi']
+handler.help = ['viadana']
 handler.tags = ['info']
-handler.command = /^viadigi$/i
+handler.command = /^viadana$/i
 
 module.exports = handler
