53e656402ada2822d386019b5b1d6e0a49bc4b93
AhmadBOTV3z <101960570+AhmadBOTV3z@users.noreply.github.com>
2022-04-02 13:20:11 +0800

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -68,7 +68,8 @@ handler.all = async function (m, { isBlocked }) {
     if (new Date() * 1 - setting.status > 1000) {
         let _uptime = process.uptime() * 1000
         let uptime = clockString(_uptime)
-        await this.setStatus(`Aktif selama ${uptime} ⏲️| Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} 👾|Botz by AhmadBOT😎`).catch(_ => _)
+        await this.setStatus(`Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} |Botz by AhmadBOT
+`).catch(_ => _)
         setting.status = new Date() * 1
     }
 
