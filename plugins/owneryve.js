1ac52289035252e72b076d85054a93b03bd64b74
gilbranp <102883231+gilbranp@users.noreply.github.com>
2022-04-03 10:58:05 +0700

    Update owneryve.js

--- plugins/owneryve.js
+++ plugins/owneryve.js
@@ -1,5 +1,5 @@
 let handler = function (m) {
-	this.sendContact(m.chat, '6281250521455', 'Owner Bot Ahmad :)', m)
+	this.sendContact(m.chat, '6281268477296', 'Owner Bot Neroh :)', m)
 }
 
 handler.customPrefix = ['Owner bot'] 
