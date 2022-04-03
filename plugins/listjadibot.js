1003720594c67dc2d94fa45992518c41c7eafa14
gilbranp <102883231+gilbranp@users.noreply.github.com>
2022-04-03 13:48:48 +0700

    Update listjadibot.js

--- plugins/listjadibot.js
+++ plugins/listjadibot.js
@@ -1,6 +1,6 @@
 async function handler(m, { usedPrefix }) {
   let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user)])]
-  m.reply( `🌟 *Bot Utama :*\n⤷ wa.me/6281250521455?text=.menu (AhmadBOT)\n\n`+ `🤖 *JadiBot :*\n` + users.map(v => '✎ wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=${usedPrefix}menu (${v.name})`).join('\n'))
+  m.reply( `🌟 *Bot Utama :*\n⤷ wa.me/6282225232861?text=.menu (NerohBOT)\n\n`+ `🤖 *JadiBot :*\n` + users.map(v => '✎ wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=${usedPrefix}menu (${v.name})`).join('\n'))
 }
 handler.command = handler.help = ['listjadibot','listbot','ljb']
 handler.tags = ['jadibot']
